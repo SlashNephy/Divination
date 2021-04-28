0397fb73535d0949e6f192ce994578d75ea889f9
Nep <spica@starry.blue>
2021-04-28 13:15:44 +0900

    Fix comment

--- Dalamud.Divination.Common/Api/IDivinationPluginApi.cs
+++ Dalamud.Divination.Common/Api/IDivinationPluginApi.cs
@@ -4,7 +4,7 @@ using Dalamud.Plugin;
 namespace Dalamud.Divination.Common.Api
 {
     /// <summary>
-    /// 各 Divination プラグインが実装している
+    /// 各 Divination プラグインが実装している各種 API のインターフェイスです。
     /// </summary>
     public interface IDivinationPluginApi<out TC> where TC : class, IPluginConfiguration
     {
