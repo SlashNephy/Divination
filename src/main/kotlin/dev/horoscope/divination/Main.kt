0b0fb091e6b46cda5c0671872f9ac2a620abaa2e
Nep <spica@starry.blue>
2021-08-01 15:01:47 +0900

    Better shutdown hook

--- src/main/kotlin/dev/horoscope/divination/Main.kt
+++ src/main/kotlin/dev/horoscope/divination/Main.kt
@@ -4,32 +4,29 @@ import dev.horoscope.divination.core.SseEventBus
 import dev.horoscope.divination.primitives.SseEvent
 import io.ktor.application.Application
 import io.ktor.server.cio.CIO
+import io.ktor.server.engine.addShutdownHook
 import io.ktor.server.engine.embeddedServer
 import kotlinx.coroutines.delay
 import mu.KotlinLogging
-import kotlin.concurrent.thread
 import kotlin.time.Duration
 
 suspend fun main() {
-    embeddedServer(
+    val engine = embeddedServer(
         factory = CIO,
         port = Env.HTTP_PORT ?: 9090,
         host = Env.HTTP_HOST ?: "0.0.0.0",
         module = Application::module
-    ).start()
-
-    registerShutdownHandler()
-    broadcastPingForever()
-}
-
-private fun registerShutdownHandler() {
-    Runtime.getRuntime().addShutdownHook(thread(start = false) {
+    )
+    engine.addShutdownHook {
         val logger = KotlinLogging.create("Divination.SseServer")
         logger.info { "Received SIGINT signal." }
 
         SseEventBus.tryBroadcast(SseEvent.Maintenance)
         Thread.sleep(5000)
-    })
+    }
+    engine.start()
+
+    broadcastPingForever()
 }
 
 private suspend fun broadcastPingForever() {
