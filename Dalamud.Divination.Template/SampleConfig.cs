13d360b486f75d3e6dcae36987a6ea650a8854ba
Nep <spica@starry.blue>
2021-09-03 13:30:42 +0900

    Update template

--- Dalamud.Divination.Template/SampleConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dalamud.Configuration;
-
-namespace Dalamud.Divination.Template
-{
-    public class SampleConfig : IPluginConfiguration
-    {
-        public int Version { get; set; } = 1;
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Dalamud.Divination.Template
+
 📝 Dalamud.Divination プラグインのテンプレート
+
+## Common の更新
+
+```shell
+# Submodule の追従
+$ git submodule foreach git pull origin master
+```
