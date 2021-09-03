f4f7a981466f8f907caf6b7815980403eaf7f68d
Nep <spica@starry.blue>
2021-09-03 19:11:02 +0900

    Initial commit

---
+++ Divination.Template/TemplatePlugin.cs
@@ -0,0 +1,27 @@
+﻿using Dalamud.Divination.Common.Api.Command;
+using Dalamud.Divination.Common.Api.Ui.Window;
+using Dalamud.Divination.Common.Boilerplate;
+using Dalamud.Divination.Common.Boilerplate.Features;
+using Dalamud.Plugin;
+
+namespace Divination.Template
+{
+    public class TemplatePlugin : DivinationPlugin<TemplatePlugin, PluginConfig, PluginDefinition>,
+        IDalamudPlugin, ICommandSupport, IConfigWindowSupport<PluginConfig>, IDefinitionSupport
+    {
+        public TemplatePlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        {
+            Logger.Information("Plugin loaded!");
+        }
+
+        public string MainCommandPrefix => "/template";
+        public string DefinitionUrl => "https://raw.githubusercontent.com/SlashNephy/Dalamud.Divination.Ephemera/master/dist/Template.json";
+        public ConfigWindow<PluginConfig> CreateConfigWindow() => new PluginConfigWindow();
+
+        [Command("hoge")]
+        private void OnHogeCommand(CommandContext context)
+        {
+            Logger.Information("/template hoge called");
+        }
+    }
+}
