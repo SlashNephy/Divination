e9cb1315ac1b829e9a6bde5e9887aed90beef7cd
Nep <spica@starry.blue>
2021-10-09 17:49:57 +0900

    Fix for Dalamud 6

--- Divination.Calculator/CalculatorPlugin.cs
+++ Divination.Calculator/CalculatorPlugin.cs
@@ -1,8 +1,10 @@
 ﻿using System.Data;
 using Dalamud.Divination.Common.Api.Command;
+using Dalamud.Divination.Common.Api.Command.Attributes;
 using Dalamud.Divination.Common.Boilerplate;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Dalamud.Logging;
 using Dalamud.Plugin;
 
 namespace Divination.Calculator
@@ -11,18 +13,19 @@ namespace Divination.Calculator
     {
         public CalculatorPlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
         {
-            Logger.Information("Plugin loaded!");
+            PluginLog.Information("Plugin loaded!");
         }
 
-        [Command("/calc", "text", Help = "数式 <text> を計算し, 結果を表示します。", Strict = false)]
+        [Command("/calc", "<text...>")]
+        [CommandHelp("数式 <text...> を計算し, 結果を表示します。基本的な四則演算をサポートしています。")]
         private void OnCalcCommand(CommandContext context)
         {
             var result = new DataTable()
-                .Compute(context.ArgumentText, null)
+                .Compute(context["text"], null)
                 .ToString() ?? string.Empty;
 
             Divination.Chat.Print(new SeString(
-                new TextPayload($"{context.ArgumentText} = "),
+                new TextPayload($"{context["text"]} = "),
                 EmphasisItalicPayload.ItalicsOn,
                 new TextPayload(result),
                 EmphasisItalicPayload.ItalicsOff));
