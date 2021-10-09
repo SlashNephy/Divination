a3e4b519ad10bb45c75b7840685b531cf5a22faf
Nep <spica@starry.blue>
2021-10-09 16:54:14 +0900

    Add logging around ServiceContainer

--- Dalamud.Divination.Common/Api/ServiceContainer.cs
+++ Dalamud.Divination.Common/Api/ServiceContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Dalamud.Logging;
 
 namespace Dalamud.Divination.Common.Api
 {
@@ -9,26 +10,36 @@ namespace Dalamud.Divination.Common.Api
 
         public static T GetOrPut<T>(Func<T> initializer) where T : class
         {
-            if (Services.TryGetValue(typeof(T), out var service))
+            lock (Services)
             {
-                return service as T ?? throw new InvalidCastException("Cast failed.");
-            }
+                if (Services.TryGetValue(typeof(T), out var service))
+                {
+                    PluginLog.Debug($"Get<{typeof(T)}>: {service?.GetType()}");
+                    return service as T ?? throw new InvalidCastException("Cast failed.");
+                }
 
-            var newService = initializer();
-            Services[typeof(T)] = newService;
-            return newService;
+                var newService = initializer();
+                Services[typeof(T)] = newService;
+                PluginLog.Debug($"Put<{typeof(T)}>: {newService.GetType()}");
+                return newService;
+            }
         }
 
         public static T? GetOrPutOptional<T>(Func<T?> initializer) where T : class?
         {
-            if (Services.TryGetValue(typeof(T), out var service))
+            lock (Services)
             {
-                return service as T;
-            }
+                if (Services.TryGetValue(typeof(T), out var service))
+                {
+                    PluginLog.Debug($"Get<{typeof(T)}>: {service?.GetType()}");
+                    return service as T;
+                }
 
-            var newService = initializer();
-            Services[typeof(T)] = newService;
-            return newService;
+                var newService = initializer();
+                Services[typeof(T)] = newService;
+                PluginLog.Debug($"Put<{typeof(T)}>: {newService?.GetType()}");
+                return newService;
+            }
         }
     }
 }
