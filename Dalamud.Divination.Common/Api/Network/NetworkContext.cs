047940cd5412a437ee9c54e66775f33ffd40d227
Nep <spica@starry.blue>
2022-04-03 19:38:12 +0900

    feat: restore NetworkContext#ToString

--- Dalamud.Divination.Common/Api/Network/NetworkContext.cs
+++ Dalamud.Divination.Common/Api/Network/NetworkContext.cs
@@ -17,5 +17,49 @@ namespace Dalamud.Divination.Common.Api.Network
 
         public byte this[int i] => Data[i];
         public IpcHeader Header => RawHeader.ToStructure<IpcHeader>();
+
+        public string ToString(int length)
+        {
+            var data = Data.Take(length).ToArray();
+
+            var message = $"{(Direction == NetworkMessageDirection.ZoneUp ? "S" : "R")}{Time:HH:mm:ss.fff}";
+            for (var i = 0; i < data.Length; i++)
+            {
+                message += $"({i:D2})";
+            }
+
+            message += $"{Environment.NewLine}    |0x{Opcode:X4}|  {BitConverter.ToString(data).Replace("-", "--")}";
+
+            // byte
+            message += $"{Environment.NewLine}    | {Opcode,5}|";
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (int val in data)
+            {
+                message += $" {val,3}";
+            }
+
+            // ushort
+            message += $"{Environment.NewLine}            ";
+            for (var i = 0; i < data.Length / 2; i++)
+            {
+                var val = BitConverter.ToUInt16(data, i * 2);
+                message += $"  {val,6}";
+            }
+
+            // uint
+            message += $"{Environment.NewLine}            ";
+            for (var i = 0; i < data.Length / 4; i++)
+            {
+                var val = BitConverter.ToUInt32(data, i * 4);
+                message += $"  {val,14}";
+            }
+
+            return message;
+        }
+
+        public override string ToString()
+        {
+            return ToString(128);
+        }
     }
 }
