66548730ba2b633f1f86d1de7deab68434fc9f24
SlashNephy <spica@starry.blue>
2022-08-24 22:49:10 +0900

    IoC をプラグイン本体にインジェクトさせる

--- Dalamud.Divination.Common/Boilerplate/DivinationPlugin{TPlugin,TConfiguration,TDefinition}.cs
+++ Dalamud.Divination.Common/Boilerplate/DivinationPlugin{TPlugin,TConfiguration,TDefinition}.cs
@@ -2,9 +2,31 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Dalamud.Configuration;
+using Dalamud.Data;
 using Dalamud.Divination.Common.Api;
 using Dalamud.Divination.Common.Api.Dalamud;
 using Dalamud.Divination.Common.Api.Definition;
+using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.ClientState.Aetherytes;
+using Dalamud.Game.ClientState.Buddy;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Game.ClientState.Fates;
+using Dalamud.Game.ClientState.GamePad;
+using Dalamud.Game.ClientState.JobGauge;
+using Dalamud.Game.ClientState.Keys;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Game.Gui.Dtr;
+using Dalamud.Game.Gui.FlyText;
+using Dalamud.Game.Gui.PartyFinder;
+using Dalamud.Game.Gui.Toast;
+using Dalamud.Game.Libc;
+using Dalamud.Game.Network;
+using Dalamud.Interface;
+using Dalamud.IoC;
 using Dalamud.Logging;
 using Dalamud.Plugin;
 
@@ -24,32 +46,140 @@ namespace Dalamud.Divination.Common.Boilerplate
         where TConfiguration : class, IPluginConfiguration, new()
         where TDefinition : DefinitionContainer, new()
     {
-        protected DivinationPlugin(DalamudPluginInterface pluginInterface)
+#pragma warning disable CS8618
+        /// <summary>
+        ///     プラグインのインスタンスの静的プロパティ。
+        /// </summary>
+        public static TPlugin Instance { get; private set; }
+
+        protected DivinationPlugin()
+#pragma warning restore CS8618
         {
             Instance = this as TPlugin ?? throw new TypeAccessException("クラス インスタンスが型パラメータ: TPlugin と一致しません。");
             IsDisposed = false;
-            Dalamud = new DalamudApi(pluginInterface);
-            Divination = new DivinationApi<TConfiguration, TDefinition>(Dalamud, Assembly, this);
+            Divination = new DivinationApi<TConfiguration, TDefinition>(Assembly, this);
 
             PluginLog.Information("プラグイン: {Name} の初期化に成功しました。バージョン = {Version}",
                 Name,
                 Divination.Version.Plugin.InformationalVersion);
         }
 
-        /// <summary>
-        ///     プラグインのインスタンスの静的プロパティ。
-        /// </summary>
-#pragma warning disable 8618
-        public static TPlugin Instance { get; private set; }
-#pragma warning restore 8618
-
         public string Name => $"Divination.{Instance.GetType().Name.Replace("Plugin", string.Empty)}";
         public bool IsDisposed { get; private set; }
         public TConfiguration Config => Divination.Config.Config;
         public TDefinition? Definition => Divination.Definition?.Container;
-        public IDalamudApi Dalamud { get; }
+        [Obsolete("いずれ廃止する、同じプロパティはプラグインクラスに定義されている")]
+        public IDalamudApi Dalamud => this;
         public IDivinationApi<TConfiguration, TDefinition> Divination { get; }
         public Assembly Assembly => Instance.GetType().Assembly;
+
+        #region IoC
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public DalamudPluginInterface PluginInterface { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public DataManager DataManager { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public ChatHandlers ChatHandlers { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public Framework Framework { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public SigScanner SigScanner { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public ClientState ClientState { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public AetheryteList AetheryteList { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public BuddyList BuddyList { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public Condition Condition { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public FateTable FateTable { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public GamepadState GamepadState { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public JobGauges JobGauges { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public KeyState KeyState { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public ObjectTable ObjectTable { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public TargetManager TargetManager { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public PartyList PartyList { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public CommandManager CommandManager { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public ChatGui ChatGui { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public GameGui GameGui { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public DtrBar DtrBar { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public FlyTextGui FlyTextGui { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public PartyFinderGui PartyFinderGui { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public ToastGui ToastGui { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public LibcFunction LibcFunction { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public GameNetwork GameNetwork { get; private set; }
+
+        [PluginService]
+        [RequiredVersion("1.0")]
+        public TitleScreenMenu TitleScreenMenu { get; private set; }
+
+        #endregion
 
         #region IDisposable
 
--- Dalamud.Divination.Common/Boilerplate/DivinationPlugin{TPlugin}.cs
+++ Dalamud.Divination.Common/Boilerplate/DivinationPlugin{TPlugin}.cs
@@ -1,11 +1,9 @@
-﻿using Dalamud.Plugin;
-
-namespace Dalamud.Divination.Common.Boilerplate
+﻿namespace Dalamud.Divination.Common.Boilerplate
 {
     public abstract class DivinationPlugin<TPlugin> : DivinationPlugin<TPlugin, EmptyConfig, EmptyDefinitionContainer>
         where TPlugin : DivinationPlugin<TPlugin, EmptyConfig, EmptyDefinitionContainer>
     {
-        protected DivinationPlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        protected DivinationPlugin() : base()
         {
         }
     }
