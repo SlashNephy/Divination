d71ba7d75337cb76aa737a0da1618331c85bb2fd
SlashNephy <spica@starry.blue>
2022-08-27 14:32:07 +0900

    ConfigWindow が PluginInterface を持つように (設定の保存がしやすいように)

--- Dalamud.Divination.Common/Api/Config/IConfigManager.cs
+++ Dalamud.Divination.Common/Api/Config/IConfigManager.cs
@@ -1,10 +1,12 @@
 ﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
 
 namespace Dalamud.Divination.Common.Api.Config
 {
     public interface IConfigManager<out T> where T : IPluginConfiguration
     {
         public T Config { get; }
+        public DalamudPluginInterface Interface { get; }
 
         public bool TryUpdate(string key, string? value, bool useTts);
     }
