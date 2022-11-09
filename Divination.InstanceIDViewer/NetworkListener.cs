0c37ca65a1390eefbc55a12ba9736b76fd0323be
SlashNephy <spica@starry.blue>
2022-11-09 20:56:48 +0900

    reusable workflow を使う

---
+++ Divination.InstanceIDViewer/NetworkListener.cs
@@ -0,0 +1,41 @@
+﻿using Dalamud.Divination.Common.Api.Chat;
+using Dalamud.Divination.Common.Api.Network;
+using Dalamud.Game.Text;
+
+namespace Divination.InstanceIDViewer
+{
+    public class NetworkListener : INetworkHandler
+    {
+        private readonly IChatClient chat;
+        private readonly object lastServerIdLock = new();
+        private ushort lastServerId;
+
+        public NetworkListener(IChatClient chat)
+        {
+            this.chat = chat;
+        }
+
+        public bool CanHandleReceivedMessage(NetworkContext context) => true;
+
+        public void HandleReceivedMessage(NetworkContext context)
+        {
+            var serverId = context.Header.ServerId;
+
+            lock (lastServerIdLock)
+            {
+                if (serverId == default || serverId == lastServerId)
+                {
+                    return;
+                }
+
+                chat.Print(
+                    $"[InstanceIDViewer] instance id changed: {lastServerId.ToString()} {SeIconChar.ArrowRight.ToIconString()} {serverId.ToString()}");
+
+                if (serverId != 0)
+                {
+                    lastServerId = serverId;
+                }
+            }
+        }
+    }
+}
