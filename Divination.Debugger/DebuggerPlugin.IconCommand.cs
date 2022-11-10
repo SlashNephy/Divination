fa46383607380735228c2ee93c7a453899067f4e
SlashNephy <spica@starry.blue>
2022-11-10 20:47:56 +0900

    SeIconChar も表示可能にする

--- Divination.Debugger/DebuggerPlugin.IconCommand.cs
+++ Divination.Debugger/DebuggerPlugin.IconCommand.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Dalamud.Divination.Common.Api.Chat;
 using Dalamud.Divination.Common.Api.Command.Attributes;
+using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
 
@@ -15,7 +16,7 @@ namespace Divination.Debugger
         {
             Divination.Chat.Print(payloads =>
             {
-                payloads.Add(new TextPayload("Icon fonts:\n"));
+                payloads.Add(new TextPayload("BitmapFontIcon:\n"));
 
                 foreach (var (i, icon) in Enum.GetValues(typeof(BitmapFontIcon)).Cast<BitmapFontIcon>().Select((x, i) => (i, x)))
                 {
@@ -23,6 +24,20 @@ namespace Divination.Debugger
                     {
                         new TextPayload($"{i:D3}"),
                         new IconPayload(icon),
+                        new TextPayload((i + 1) % 5 == 0 ? "\n" : " "),
+                    });
+                }
+
+                payloads.Add(new TextPayload("\nSeIconChar:\n"));
+
+                foreach (var (i, icon) in Enum.GetValues(typeof(SeIconChar))
+                    .Cast<SeIconChar>()
+                    .Select((x, i) => (i, x)))
+                {
+                    payloads.AddRange(new Payload[]
+                    {
+                        new TextPayload($"{i:D3}"),
+                        new TextPayload($"{(char) icon}"),
                         new TextPayload((i + 1) % 5 == 0 ? "\n" : " "),
                     });
                 }
