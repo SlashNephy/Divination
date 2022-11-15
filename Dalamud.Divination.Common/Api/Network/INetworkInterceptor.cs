313febd2022c07b1fc129a63f1f8f9517f41e874
SlashNephy <spica@starry.blue>
2022-11-16 00:20:17 +0900

    インターフェースに含めてなかった

--- Dalamud.Divination.Common/Api/Network/INetworkInterceptor.cs
+++ Dalamud.Divination.Common/Api/Network/INetworkInterceptor.cs
@@ -4,6 +4,7 @@ namespace Dalamud.Divination.Common.Api.Network
 {
     public interface INetworkInterceptor : IDisposable
     {
+        public OpcodeDetectorManager OpcodeDetectorManager { get; }
         public void AddHandler(INetworkHandler handler);
         public void RemoveHandler(INetworkHandler handler);
     }
