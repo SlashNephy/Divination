bc5dce5479953efbb67fd8c8c8b1a7165414fba9
SlashNephy <spica@starry.blue>
2022-11-21 21:01:55 +0900

    Initial commit

---
+++ Divination.Template/TemplatePlugin.cs
@@ -0,0 +1,53 @@
+﻿using Dalamud.Divination.Common.Api.Command;
+using Dalamud.Divination.Common.Api.Command.Attributes;
+using Dalamud.Divination.Common.Api.Ui.Window;
+using Dalamud.Divination.Common.Boilerplate;
+using Dalamud.Divination.Common.Boilerplate.Features;
+using Dalamud.Logging;
+using Dalamud.Plugin;
+
+namespace Divination.Template
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public sealed class TemplatePlugin : DivinationPlugin<TemplatePlugin, PluginConfig, PluginDefinition>,
+        IDalamudPlugin,
+        ICommandSupport,
+        IConfigWindowSupport<PluginConfig>,
+        IDefinitionSupport
+    {
+        public TemplatePlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        {
+            Config = pluginInterface.GetPluginConfig() as PluginConfig ?? new PluginConfig();
+        }
+
+        protected override void ReleaseManaged()
+        {
+            Dalamud.PluginInterface.SavePluginConfig(Config);
+        }
+
+        #region ICommandSupport
+
+        public string MainCommandPrefix => "/template";
+
+        [Command("/example", "foo", "<arg>", "<optionalarg?>", "<vararg...>")]
+        [CommandHelp("This is sample command.")]
+        private static void OnExampleCommand(CommandContext context)
+        {
+            PluginLog.Information("/example called.");
+        }
+
+        #endregion
+
+        #region IConfigWindowSupport
+
+        public ConfigWindow<PluginConfig> CreateConfigWindow() => new PluginConfigWindow();
+
+        #endregion
+
+        #region IDefinitionSupport
+
+        public string DefinitionUrl => "https://horoscope-dev.github.io/Dalamud.Divination.Ephemera/dist/Template.json";
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Dalamud.Divination.Template
+
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/horoscope-dev/Dalamud.Divination.Template/CI?style=flat-square)](https://github.com/horoscope-dev/Dalamud.Divination.Template/actions/workflows/ci.yml)
+
+A template for developing Divination projects.
+
+## How to use
+
+1. Click "Use this template" on GitHub.
+2. Open the solution, and rename `Divination.Template` to your preferred name.
+3. Replace all `Template` strings with your project name.
+4. Issue GitHub PAT (Private Access Token), and add it to the repository secrets
+   with the name `GH_PAT`. Please note that you must issue a PAT with write access to the plugin repository as described
+   below.
+5. Start coding!
+
+## Distribution
+
+This repository is automatically built by GitHub Actions and published to the plugin repository (default:
+[Dalamud.DivinationPluginRepo](https://github.com/horoscope-dev/Dalamud.DivinationPluginRepo)).
+
+When a release tag is created, it is published to the stable repository, and when a push to `master` branch is made, it is
+published to the testing repository.
