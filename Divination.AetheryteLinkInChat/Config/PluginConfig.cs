2bb099b8df84cb349e96841c3c17b6163faf7fe2
SlashNephy <spica@starry.blue>
2022-12-13 00:04:58 +0900

    テレポをキュー可能に (Close #16)

---
+++ Divination.AetheryteLinkInChat/Config/PluginConfig.cs
@@ -0,0 +1,12 @@
+﻿using Dalamud.Configuration;
+
+namespace Divination.AetheryteLinkInChat.Config;
+
+public class PluginConfig : IPluginConfiguration
+{
+    public int Version { get; set; }
+
+    public bool PrintAetheryteName;
+    public bool AllowTeleportQueueing;
+    public int QueuedTeleportDelay = 2000;
+}
---
+++ Divination.AetheryteLinkInChat/Config/PluginConfigWindow.cs
@@ -0,0 +1,35 @@
+﻿using Dalamud.Divination.Common.Api.Ui.Window;
+using ImGuiNET;
+
+namespace Divination.AetheryteLinkInChat.Config;
+
+public class PluginConfigWindow : ConfigWindow<PluginConfig>
+{
+    public override void Draw()
+    {
+        if (ImGui.Begin($"{AetheryteLinkInChatPlugin.Instance.Name} Config", ref IsOpen,
+            ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoResize))
+        {
+            ImGui.Checkbox("Print Aetheryte Name to Chat", ref Config.PrintAetheryteName);
+
+            ImGui.Checkbox("Allow Teleport Queueing", ref Config.AllowTeleportQueueing);
+            if (Config.AllowTeleportQueueing)
+            {
+                ImGui.SliderInt("Queued Teleport Delay (ms)", ref Config.QueuedTeleportDelay, 500, 5000);
+            }
+
+            ImGui.Indent();
+            ImGui.Text("It can queue when teleport is unavailable, e.g. in combat.");
+            ImGui.Text("Then your teleport will be executed after the Delay.");
+            ImGui.Unindent();
+
+            if (ImGui.Button("Save & Close"))
+            {
+                IsOpen = false;
+                Interface.SavePluginConfig(Config);
+            }
+
+            ImGui.End();
+        }
+    }
+}
