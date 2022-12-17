1f160cbf1337ad520d653d6310f3b4d9c8c0947a
SlashNephy <spica@starry.blue>
2022-12-17 21:48:20 +0900

    Localize

--- Dalamud.Divination.Common/Api/ServiceContainer.cs
+++ Dalamud.Divination.Common/Api/ServiceContainer.cs
@@ -3,60 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Logging;
 
-namespace Dalamud.Divination.Common.Api
+namespace Dalamud.Divination.Common.Api;
+
+internal static class ServiceContainer
 {
-    internal static class ServiceContainer
+    private static readonly List<(Type type, object? instance)> Services = new();
+
+    public static T? Get<T>() where T : class
     {
-        private static readonly List<(Type type, object? instance)> Services = new();
-
-        public static T GetOrPut<T>(Func<T> initializer) where T : class
+        lock (Services)
         {
-            return GetOrPutOptional(initializer) ?? throw new InvalidCastException("Cast failed.");
-        }
-
-        public static T? GetOrPutOptional<T>(Func<T?> initializer) where T : class?
-        {
-            lock (Services)
+            var entry = Services.FirstOrDefault(x => x.type == typeof(T));
+            if (entry == default)
             {
-                var entry = Services.FirstOrDefault(x => x.type == typeof(T));
-                // PluginLog.Verbose($"Get<{typeof(T)}>: {entry.instance?.GetType()}");
+                return default;
+            }
 
-                if (entry == default)
+            return entry.instance as T;
+        }
+    }
+
+    public static void Put<T>(T instance) where T : class
+    {
+        lock (Services)
+        {
+            Services.Add((typeof(T), instance));
+        }
+    }
+
+    public static T GetOrPut<T>(Func<T> initializer) where T : class
+    {
+        return GetOrPutOptional(initializer) ?? throw new InvalidCastException("Cast failed.");
+    }
+
+    public static T? GetOrPutOptional<T>(Func<T?> initializer) where T : class?
+    {
+        lock (Services)
+        {
+            var entry = Services.FirstOrDefault(x => x.type == typeof(T));
+            // PluginLog.Verbose($"Get<{typeof(T)}>: {entry.instance?.GetType()}");
+
+            if (entry == default)
+            {
+                entry = (typeof(T), initializer());
+                Services.Add(entry);
+                PluginLog.Verbose($"Put<{entry.type}>: {entry.instance?.GetType()}");
+            }
+
+            return entry.instance as T;
+        }
+    }
+
+    public static bool Contains<T>() where T : class
+    {
+        lock (Services)
+        {
+            return Services.Any(x => x.type == typeof(T));
+        }
+    }
+
+    public static void DestroyAll()
+    {
+        // 追加された順の逆順で破棄する
+        lock (Services)
+        {
+            foreach (var (type, instance) in Services)
+            {
+                if (instance is IDisposable disposable)
                 {
-                    entry = (typeof(T), initializer());
-                    Services.Add(entry);
-                    PluginLog.Verbose($"Put<{entry.type}>: {entry.instance?.GetType()}");
+                    disposable.Dispose();
+                    PluginLog.Verbose("Dispose: {Disposable}", type.FullName!);
                 }
-
-                return entry.instance as T;
             }
-        }
 
-        public static bool Contains<T>() where T : class
-        {
-            lock (Services)
-            {
-                return Services.Any(x => x.type == typeof(T));
-            }
-        }
-
-        public static void DestroyAll()
-        {
-            // 追加された順の逆順で破棄する
-            lock (Services)
-            {
-                foreach (var (type, instance) in Services)
-                {
-                    if (instance is IDisposable disposable)
-                    {
-                        disposable.Dispose();
-                        PluginLog.Verbose("Dispose: {Disposable}", type.FullName!);
-                    }
-                }
-
-                Services.Clear();
-                PluginLog.Verbose("ServiceContainer is clear");
-            }
+            Services.Clear();
+            PluginLog.Verbose("ServiceContainer is clear");
         }
     }
 }
+
