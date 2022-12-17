7eb3c37f90aa7a51f639da1005a82ceb8b5ef7f0
SlashNephy <spica@starry.blue>
2022-12-17 21:48:20 +0900

    Localize

---
+++ Dalamud.Divination.Common/Api/Localize/LocalizedString.cs
@@ -0,0 +1,55 @@
+﻿using Dalamud.Divination.Common.Api.Dalamud;
+
+namespace Dalamud.Divination.Common.Api.Localize;
+
+public sealed class LocalizedString
+{
+    private string? cached;
+
+    public string En { get; init; } = string.Empty;
+    public string? Ja { get; init; }
+
+    public static explicit operator string(LocalizedString localizedString)
+    {
+        return localizedString.ToString();
+    }
+
+    public override string ToString()
+    {
+        if (cached != default)
+        {
+            return cached;
+        }
+
+        var api = ServiceContainer.Get<DalamudApi>();
+        switch (api?.ClientState.ClientLanguage)
+        {
+            case ClientLanguage.Japanese when !string.IsNullOrWhiteSpace(Ja):
+                cached = Ja;
+                return Ja;
+            default:
+                cached = En;
+                return En;
+        }
+    }
+
+    public string Format(object? arg0)
+    {
+        return string.Format(ToString(), arg0);
+    }
+
+    public string Format(object? arg0, object? arg1)
+    {
+        return string.Format(ToString(), arg0, arg1);
+    }
+
+    public string Format(object? arg0, object? arg1, object? arg2)
+    {
+        return string.Format(ToString(), arg0, arg1, arg2);
+    }
+
+    public string Format(params object?[] args)
+    {
+        return string.Format(ToString(), args);
+    }
+}
