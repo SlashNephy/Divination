d241b40e229d4d4c3d6e6ea06b41256bbcc29aef
SlashNephy <spica@starry.blue>
2022-12-25 02:07:08 +0900

    デフォルトコマンド追加時のメッセージを消す

--- Dalamud.Divination.Common/Api/Command/CommandProcessor.Commands.cs
+++ Dalamud.Divination.Common/Api/Command/CommandProcessor.Commands.cs
@@ -6,41 +6,41 @@ using Dalamud.Divination.Common.Api.Dalamud.Payload;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
 
-namespace Dalamud.Divination.Common.Api.Command
+namespace Dalamud.Divination.Common.Api.Command;
+
+internal sealed partial class CommandProcessor
 {
-    internal sealed partial class CommandProcessor
+    public class DefaultCommands : ICommandProvider
     {
-        public class DefaultCommands : ICommandProvider
+        private readonly CommandProcessor processor;
+
+        public DefaultCommands(CommandProcessor processor)
         {
-            private readonly CommandProcessor processor;
+            this.processor = processor;
+        }
 
-            public DefaultCommands(CommandProcessor processor)
+        [Command("help")]
+        [HiddenCommand(HideInHelp = false)]
+        [CommandHelp("{Name} のコマンド一覧を表示します。")]
+        private void OnHelpCommand()
+        {
+            processor.chatClient.Print(payloads =>
             {
-                this.processor = processor;
-            }
+                payloads.Add(new TextPayload($"{processor.pluginName} のコマンド一覧:\n"));
 
-            [Command("help")]
-            [CommandHelp("{Name} のコマンド一覧を表示します。")]
-            private void OnHelpCommand()
-            {
-                processor.chatClient.Print(payloads =>
+                foreach (var command in processor.Commands.Where(x => !x.HideInHelp))
                 {
-                    payloads.Add(new TextPayload($"{processor.pluginName} のコマンド一覧:\n"));
+                    payloads.AddRange(PayloadUtilities.HighlightAngleBrackets(command.Usage));
 
-                    foreach (var command in processor.Commands.Where(x => !x.HideInHelp))
+                    if (!string.IsNullOrEmpty(command.Help))
                     {
-                        payloads.AddRange(PayloadUtilities.HighlightAngleBrackets(command.Usage));
-
-                        if (!string.IsNullOrEmpty(command.Help))
-                        {
-                            payloads.Add(new TextPayload($"\n {SeIconChar.ArrowRight.AsString()} "));
-                            payloads.AddRange(PayloadUtilities.HighlightAngleBrackets(command.Help));
-                        }
-
-                        payloads.Add(new TextPayload("\n"));
+                        payloads.Add(new TextPayload($"\n {SeIconChar.ArrowRight.AsString()} "));
+                        payloads.AddRange(PayloadUtilities.HighlightAngleBrackets(command.Help));
                     }
-                });
-            }
+
+                    payloads.Add(new TextPayload("\n"));
+                }
+            });
         }
     }
 }
