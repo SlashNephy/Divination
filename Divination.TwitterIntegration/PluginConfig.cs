229ff028c3b4f8449763ec42760fbb703399adef
SlashNephy <spica@starry.blue>
2022-12-27 21:43:50 +0900

    file-scoped namespace

--- Divination.TwitterIntegration/PluginConfig.cs
+++ Divination.TwitterIntegration/PluginConfig.cs
@@ -1,19 +1,18 @@
 ﻿using Dalamud.Configuration;
 
-namespace Divination.TwitterIntegration
+namespace Divination.TwitterIntegration;
+
+public class PluginConfig : IPluginConfiguration
 {
-    public class PluginConfig : IPluginConfiguration
-    {
-        public string ConsumerKey = string.Empty;
-        public string ConsumerSecret = string.Empty;
-        public string AccessToken = string.Empty;
-        public string AccessTokenSecret = string.Empty;
+    public string ConsumerKey = string.Empty;
+    public string ConsumerSecret = string.Empty;
+    public string AccessToken = string.Empty;
+    public string AccessTokenSecret = string.Empty;
 
-        public bool ShowListTimeline = false;
-        public string ListId = string.Empty;
-        public int UpdateIntervalInMs = 3000;
-        public long? SinceId = null;
+    public bool ShowListTimeline = false;
+    public string ListId = string.Empty;
+    public int UpdateIntervalInMs = 3000;
+    public long? SinceId = null;
 
-        public int Version { get; set; } = 0;
-    }
+    public int Version { get; set; } = 0;
 }
--- Divination.TwitterIntegration/TwitterIntegrationPlugin.cs
+++ Divination.TwitterIntegration/TwitterIntegrationPlugin.cs
@@ -14,111 +14,110 @@ using Dalamud.Game.Text.SeStringHandling.Payloads;
 using Dalamud.Logging;
 using Dalamud.Plugin;
 
-namespace Divination.TwitterIntegration
+namespace Divination.TwitterIntegration;
+
+public class TwitterIntegrationPlugin : DivinationPlugin<TwitterIntegrationPlugin, PluginConfig>,
+    IDalamudPlugin, ICommandSupport, IConfigWindowSupport<PluginConfig>
 {
-    public class TwitterIntegrationPlugin : DivinationPlugin<TwitterIntegrationPlugin, PluginConfig>,
-        IDalamudPlugin, ICommandSupport, IConfigWindowSupport<PluginConfig>
+    public TwitterIntegrationPlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
     {
-        public TwitterIntegrationPlugin(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        Config = pluginInterface.GetPluginConfig() as PluginConfig ?? new PluginConfig();
+        Task.Run(WatchTimeline);
+    }
+
+    private Tokens? twitter;
+    public static Tokens? Twitter
+    {
+        get
         {
-            Config = pluginInterface.GetPluginConfig() as PluginConfig ?? new PluginConfig();
-            Task.Run(WatchTimeline);
+            Instance.twitter ??= CreateTokens();
+            return Instance.twitter;
+        }
+    }
+
+    private static Tokens? CreateTokens()
+    {
+        if (string.IsNullOrEmpty(Instance.Config.ConsumerKey)
+            || string.IsNullOrEmpty(Instance.Config.ConsumerSecret)
+            || string.IsNullOrEmpty(Instance.Config.AccessToken)
+            || string.IsNullOrEmpty(Instance.Config.AccessTokenSecret))
+        {
+            return null;
         }
 
-        private Tokens? twitter;
-        public static Tokens? Twitter
+        return Tokens.Create(
+            Instance.Config.ConsumerKey,
+            Instance.Config.ConsumerSecret,
+            Instance.Config.AccessToken,
+            Instance.Config.AccessTokenSecret);
+    }
+
+    public string MainCommandPrefix => "/twitter";
+
+    public ConfigWindow<PluginConfig> CreateConfigWindow() => new PluginConfigWindow();
+
+    [Command("/tw", "<text...>")]
+    [CommandHelp("与えられた <text...> をツイートします。")]
+    private void OnTweetCommand(CommandContext context)
+    {
+        if (twitter == null)
         {
-            get
+            Divination.Chat.PrintError("Twitter API の資格情報が設定されていません。");
+            return;
+        }
+
+        twitter.Statuses.UpdateAsync(context["text"]).ContinueWith(completed =>
+        {
+            if (completed.IsCompleted)
             {
-                Instance.twitter ??= CreateTokens();
-                return Instance.twitter;
+                Divination.Chat.Print($"「{context["text"]}」をツイートしました。");
             }
-        }
-
-        private static Tokens? CreateTokens()
-        {
-            if (string.IsNullOrEmpty(Instance.Config.ConsumerKey)
-                || string.IsNullOrEmpty(Instance.Config.ConsumerSecret)
-                || string.IsNullOrEmpty(Instance.Config.AccessToken)
-                || string.IsNullOrEmpty(Instance.Config.AccessTokenSecret))
+            else if (completed.Exception != null)
             {
-                return null;
+                Divination.Chat.PrintError("ツイートに失敗しました。");
+                PluginLog.Error(completed.Exception, "Failed to tweet");
             }
+        });
+    }
 
-            return Tokens.Create(
-                Instance.Config.ConsumerKey,
-                Instance.Config.ConsumerSecret,
-                Instance.Config.AccessToken,
-                Instance.Config.AccessTokenSecret);
-        }
-
-        public string MainCommandPrefix => "/twitter";
-
-        public ConfigWindow<PluginConfig> CreateConfigWindow() => new PluginConfigWindow();
-
-        [Command("/tw", "<text...>")]
-        [CommandHelp("与えられた <text...> をツイートします。")]
-        private void OnTweetCommand(CommandContext context)
+    private async void WatchTimeline()
+    {
+        while (!IsDisposed)
         {
-            if (twitter == null)
+            if (Config.ShowListTimeline && Twitter != null && long.TryParse(Config.ListId, out var listId))
             {
-                Divination.Chat.PrintError("Twitter API の資格情報が設定されていません。");
-                return;
-            }
+                try
+                {
+                    var result = await Twitter.Lists.StatusesAsync(listId, count: 5, since_id: Config.SinceId);
 
-            twitter.Statuses.UpdateAsync(context["text"]).ContinueWith(completed =>
-            {
-                if (completed.IsCompleted)
-                {
-                    Divination.Chat.Print($"「{context["text"]}」をツイートしました。");
-                }
-                else if (completed.Exception != null)
-                {
-                    Divination.Chat.PrintError("ツイートに失敗しました。");
-                    PluginLog.Error(completed.Exception, "Failed to tweet");
-                }
-            });
-        }
-
-        private async void WatchTimeline()
-        {
-            while (!IsDisposed)
-            {
-                if (Config.ShowListTimeline && Twitter != null && long.TryParse(Config.ListId, out var listId))
-                {
-                    try
+                    foreach (var status in result.Reverse())
                     {
-                        var result = await Twitter.Lists.StatusesAsync(listId, count: 5, since_id: Config.SinceId);
-
-                        foreach (var status in result.Reverse())
+                        Dalamud.ChatGui.PrintChat(new XivChatEntry
                         {
-                            Dalamud.ChatGui.PrintChat(new XivChatEntry
-                            {
-                                Type = XivChatType.Echo,
-                                Message = new SeString(
-                                    new UIForegroundPayload(57),
-                                    new TextPayload("[Twitter]"),
-                                    UIForegroundPayload.UIForegroundOff,
-                                    new TextPayload($"<@{status.User.ScreenName}> {HttpUtility.HtmlDecode(status.Text)}"))
-                            });
+                            Type = XivChatType.Echo,
+                            Message = new SeString(
+                                new UIForegroundPayload(57),
+                                new TextPayload("[Twitter]"),
+                                UIForegroundPayload.UIForegroundOff,
+                                new TextPayload($"<@{status.User.ScreenName}> {HttpUtility.HtmlDecode(status.Text)}"))
+                        });
 
-                            PluginLog.Verbose("Tweet = {Text} ({Id}) from {Name}", status.Text, status.Id, status.User.Name);
-                        }
-
-                        if (result.Count > 0)
-                        {
-                            Config.SinceId = result[0].Id + 1;
-                        }
+                        PluginLog.Verbose("Tweet = {Text} ({Id}) from {Name}", status.Text, status.Id, status.User.Name);
                     }
-                    catch (Exception exception)
+
+                    if (result.Count > 0)
                     {
-                        Divination.Chat.PrintError("タイムラインの取得に失敗しました。");
-                        PluginLog.Error(exception, "Failed to fetch timeline");
+                        Config.SinceId = result[0].Id + 1;
                     }
                 }
-
-                await Task.Delay(Config.UpdateIntervalInMs);
+                catch (Exception exception)
+                {
+                    Divination.Chat.PrintError("タイムラインの取得に失敗しました。");
+                    PluginLog.Error(exception, "Failed to fetch timeline");
+                }
             }
+
+            await Task.Delay(Config.UpdateIntervalInMs);
         }
     }
 }
