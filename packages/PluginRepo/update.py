e83c7181630a1c1c6a9dc3accb5e1f4fb666224f
SlashNephy <spica@starry.blue>
2023-04-27 01:48:11 +0900

    feat(PluginRepo): add CI

--- packages/PluginRepo/update.py
+++ packages/PluginRepo/update.py
@@ -1,23 +1,22 @@
 import json
 import os
-import urllib.request
 from datetime import datetime, timedelta, timezone
 from zipfile import ZipFile
 
-PROVIDER = os.getenv("PROVIDER", "https://slashnephy.github.io/Dalamud.DivinationPluginRepo/dist")
 
 def extract_manifests(env):
     manifests = {}
-    for dirpath, _, filenames in os.walk(f"dist/{env}"):
+    for dir_path, _, filenames in os.walk(f"docs/plugins/{env}"):
         if "latest.zip" not in filenames:
             continue
 
-        with ZipFile(f"{dirpath}/latest.zip") as z:
-            plugin_name = dirpath.split("/")[-1]
+        with ZipFile(f"{dir_path}/latest.zip") as z:
+            plugin_name = dir_path.split("/")[-1]
             manifest = json.loads(z.read(f"{plugin_name}.json").decode())
             manifests[manifest["InternalName"]] = manifest
 
     return manifests
+
 
 def get_changelog(path):
     commits_path = f"{path}/commits.json"
@@ -36,6 +35,7 @@ def get_changelog(path):
         if x["commit"]["author"]["name"] != "github-actions"
     ]) or None
 
+
 def get_repo_url(path):
     event_path = f"{path}/event.json"
     if not os.path.exists(event_path):
@@ -48,6 +48,7 @@ def get_repo_url(path):
         return event["repository"]["html_url"]
 
     return None
+
 
 def get_last_updated(path):
     event_path = f"{path}/event.json"
@@ -80,17 +81,18 @@ def get_last_updated(path):
         epoch = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
     return int(epoch.timestamp())
 
+
 def merge_manifests(stable, testing):
     manifest_keys = set(list(stable.keys()) + list(testing.keys()))
 
     manifests = []
     for key in manifest_keys:
-        stable_path = f"dist/stable/{key}"
+        stable_path = f"docs/plugins/stable/{key}"
         stable_manifest = stable.get(key, {})
-        stable_link = f"{PROVIDER}/stable/{key}/latest.zip"
-        testing_path = f"dist/testing/{key}"
+        stable_link = f"https://xiv.starry.blue/plugins/stable/{key}/latest.zip"
+        testing_path = f"docs/plugins/testing/{key}"
         testing_manifest = testing.get(key, {})
-        testing_link = f"{PROVIDER}/testing/{key}/latest.zip"
+        testing_link = f"https://xiv.starry.blue/plugins/testing/{key}/latest.zip"
 
         manifest = testing_manifest.copy() if testing_manifest else stable_manifest.copy()
 
@@ -103,21 +105,23 @@ def merge_manifests(stable, testing):
         manifest["LastUpdated"] = max(get_last_updated(stable_path), get_last_updated(testing_path))
         manifest["DownloadLinkInstall"] = stable_link if stable_manifest else testing_link
         manifest["DownloadLinkTesting"] = testing_link if testing_manifest else stable_link
-        manifest["IconUrl"] = testing_manifest.get("IconUrl", stable_manifest.get("IconUrl", "https://avatars.githubusercontent.com/u/7855451?v=4&s=64"))
+        manifest["IconUrl"] = testing_manifest.get("IconUrl", stable_manifest.get("IconUrl"))
 
         manifests.append(manifest)
 
     return manifests
 
+
 def dump_master(manifests):
     manifests.sort(key=lambda x: x["InternalName"])
 
-    with open("dist/pluginmaster.json", "w") as f:
+    with open("docs/plugins/master.json", "w") as f:
         json.dump(manifests, f, indent=2, sort_keys=True)
+
 
 def generate_markdown(manifests):
     lines = [
-        "# Dalamud.Divination Plugins",
+        "# Divination Plugins",
         "",
         "## Legend",
         "",
@@ -149,15 +153,14 @@ def generate_markdown(manifests):
 
         lines.append(f"| {name} | {version} | {author} | {description} |")
 
-    with open("dist/README.md", "w") as f:
+    with open("docs/plugins/README.md", "w") as f:
         f.write("\n".join(lines))
 
 
 if __name__ == "__main__":
-    stable = extract_manifests("stable")
-    testing = extract_manifests("testing")
+    stable_manifests = extract_manifests("stable")
+    testing_manifests = extract_manifests("testing")
+    merged_manifests = merge_manifests(stable_manifests, testing_manifests)
 
-    manifests = merge_manifests(stable, testing)
-    dump_master(manifests)
-
-    generate_markdown(manifests)
+    dump_master(merged_manifests)
+    generate_markdown(merged_manifests)
