362db023b13d056e276e33e1cc16bf14d29daaa5
SlashNephy <spica@starry.blue>
2024-02-21 20:23:04 +0900

    update IoC for latest Dalamud

--- packages/Common/Api/Dalamud/DalamudApi.cs
+++ packages/Common/Api/Dalamud/DalamudApi.cs
@@ -24,34 +24,17 @@ internal sealed class DalamudApi : IDalamudApi
         }
     }
 
-    public DalamudPluginInterface PluginInterface { get; }
+    public DalamudPluginInterface PluginInterface { get; private set; }
 
     #region IoC
 
     [PluginService]
     [RequiredVersion("1.0")]
-    public IDataManager DataManager { get; private set; }
+    public IAddonEventManager AddonEventManager { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
-    public ITextureProvider TextureProvider { get; private set; }
-
-    // TODO - Find something to replace chat handlers, since its private now
-    /*[PluginService]
-    [RequiredVersion("1.0")]
-    public ChatHandlers ChatHandlers { get; private set; }*/
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IFramework Framework { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public ISigScanner SigScanner { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IClientState ClientState { get; private set; }
+    public IAddonLifecycle AddonLifecycle { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
@@ -63,11 +46,64 @@ internal sealed class DalamudApi : IDalamudApi
 
     [PluginService]
     [RequiredVersion("1.0")]
+    public IChatGui ChatGui { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IClientState ClientState { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public ICommandManager CommandManager { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
     public ICondition Condition { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
+    public IDataManager DataManager { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IDtrBar DtrBar { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IDutyState DutyState { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
     public IFateTable FateTable { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IFlyTextGui FlyTextGui { get; private set; }
+    public IFramework Framework { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameConfig GameConfig { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameGui GameGui { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameInteropProvider GameInteropProvider { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameInventory GameInventory { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameLifecycle GameLifecycle { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IGameNetwork GameNetwork { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
@@ -83,35 +119,11 @@ internal sealed class DalamudApi : IDalamudApi
 
     [PluginService]
     [RequiredVersion("1.0")]
+    public ILibcFunction LibcFunction { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
     public IObjectTable ObjectTable { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public ITargetManager TargetManager { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IPartyList PartyList { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public ICommandManager CommandManager { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IChatGui ChatGui { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IGameGui GameGui { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IDtrBar DtrBar { get; private set; }
-
-    [PluginService]
-    [RequiredVersion("1.0")]
-    public IFlyTextGui FlyTextGui { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
@@ -119,19 +131,35 @@ internal sealed class DalamudApi : IDalamudApi
 
     [PluginService]
     [RequiredVersion("1.0")]
-    public IToastGui ToastGui { get; private set; }
+    public IPartyList PartyList { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
-    public ILibcFunction LibcFunction { get; private set; }
+    public IPluginLog PluginLog { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
-    public IGameNetwork GameNetwork { get; private set; }
+    public ISigScanner SigScanner { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public ITargetManager TargetManager { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public ITextureProvider TextureProvider { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public ITextureSubstitutionProvider TextureSubstitutionProvider { get; private set; }
 
     [PluginService]
     [RequiredVersion("1.0")]
     public ITitleScreenMenu TitleScreenMenu { get; private set; }
+
+    [PluginService]
+    [RequiredVersion("1.0")]
+    public IToastGui ToastGui { get; private set; }
 
     #endregion
 }
